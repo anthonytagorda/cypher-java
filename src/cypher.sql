9ee6e235c583402786f21874022f674dff76fa5f
anthonytagorda <anthtago@gmail.com>
2026-07-15 05:06:16 +0800

    updated cypher.sql (added status, isBanned, totalWIns)

--- src/cypher.sql
+++ src/cypher.sql
@@ -5,13 +5,14 @@ USE cypher;
 -- PLAYERS / AUTH
 -- =========================
 CREATE TABLE IF NOT EXISTS players (
-    player_id        INT AUTO_INCREMENT PRIMARY KEY,
-    username         VARCHAR(50) NOT NULL UNIQUE,
-    password         VARCHAR(100) NOT NULL,
-    is_online        TINYINT(1) NOT NULL DEFAULT 0,
-    is_banned        TINYINT(1) NOT NULL DEFAULT 0,
-    created_at       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
+    player_id    INT AUTO_INCREMENT PRIMARY KEY,
+    username     VARCHAR(50) NOT NULL UNIQUE,
+    password     VARCHAR(100) NOT NULL,
+    status       ENUM('online','offline','in-game') NOT NULL DEFAULT 'offline',
+    isBanned     TINYINT(1) NOT NULL DEFAULT 0,
+    totalWins    INT NOT NULL DEFAULT 0,
+    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 -- =========================
 -- GAMES
